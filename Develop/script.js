a3a0c8212e34fd10299cd9dc649f544622093742
RickyCohen <ricky.cohen88@gmail.com>
2020-10-12 21:22:37 -0500

    added to html check boxes, added to js var order and if statement for  it to reverse arrays, log exit value, added arrays for upper , lower  num and special variables, made array to be used for chosen variables. made if statements for check boxes will change to switch statement

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,35 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");// sets a variable to html button
-
 var lowercase = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!","@","#","$","%","^","&","*"];
+var usedarr = [];
+
+var generateBtn = document.querySelector("#generate");// sets a variable to html button
+
+
+var passwordrtn;
+
+// get user criteria for password
+var length = document.querySelector("#length");
+//var chklow = document.getElementById("chklow").checked;
+if( document.getElementById("chkupp").checked){
+  usedarr.push(uppercase);
+}
+if( document.getElementById("chklow").checked){
+  usedarr.push(lowercase);
+}
+if( document.getElementById("chkspec").checked){
+  usedarr.push(special);
+}
+if( document.getElementById("chknum").checked){
+  usedarr.push(numbers);
+}
 
 
 
 
-// Write password to the #password input
 
-function makeApassword1 (){
 
 let order = Math.random;
 
@@ -28,12 +46,17 @@ else {
 
 }
 
+for( let i=0; i < length;i++){
 
 
-;
+}
+function createkey (){
+
 }
 
 
+
+// Write password to the #password input
 // original content
 /*function writePassword() {
   var password = generatePassword();
