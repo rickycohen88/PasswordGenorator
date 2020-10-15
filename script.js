85fe84740fbf4203dc192f23b732ee810d21c729
RickyCohen <ricky.cohen88@gmail.com>
2020-10-15 02:48:42 -0500

    rewrote alot of code to be simpler and more streamlined, also fixed all bugs,need to implement final checks on pw to criteria having at least 1 char from each chosen subset

--- script.js
+++ script.js
@@ -1,68 +1,64 @@
 var generateBtn = document.querySelector("#generate");// sets a variable to html button
-generateBtn.addEventListener("click", trigger);//runs function when click is present on variable generatebtn
-
-
-
-
-// Assignment Code
-
-var confirmready = false; 
-var passlength = 0;
+generateBtn.addEventListener("click", definepassword);//runs function when click is present on variable generatebtn
+// Assignment Code 
+let lowercase = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numbers = ["0","1","2","3","4","5","6","7","8","9"];
+let special = ["!","@","#","$","%","^","&","*"," ",];//need more!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 var passwordrtn;
-
-function trigger(){
-  //usedarr.length = 0; //usedarr=[]
-  console.log("used arr has ben set to zero");
-  console.log(usedarr);
-  testobject.definepassword();
-}
+var passlength;
  // get user criteria for password and push to array to be used for creating password
  //function is called onclick of generate password btn on html page
- 
-
-
-    
- function definepassword () {
-  
-  document.getElementById("notifications").innerHTML=undefined;
-    
-    let passlength = document.getElementById("lengthinput").value;
-    console.log(passlength);
-    
+function definepassword () {
+ passlength = document.getElementById("lengthinput").value;
+  console.log(passlength);
     if (passlength > 128 || passlength < 8){
-      document.getElementById("notifications").innerHTML ="please specify the length of the password.";
+      document.getElementById("notifications").innerHTML ="please specify the length of the password between 8 and 128.";
       console.log("sent length error to text feild");
-      console.log(passlength);
+      
     }
-   }   
+    else if(document.getElementById("chkupp").checked == false &&  document.getElementById("chklow").checked == false  &&  document.getElementById("chknum").checked == false && document.getElementById("chkspec").checked == false){
+      document.getElementById("notifications").innerHTML ="please specify the charcters of the password to be used.";
+      console.log("sent character error to text feild");
+      console.log("no char selected");
+    }
+    else{
+        confirmready();
+        passwordrtn ="";
+        console.log(passwordrtn);
+    }
+}
+function confirmready (){
+  typeof passwordrtn ==="undefined";
+        console.log(passwordrtn);
+  let arrayofchar =[];
+ if(document.getElementById("chkupp").checked){
+   arrayofchar.push("uppercase");
    
-   function areweconfirmready (){
-console.log(confirmready);
-if (confirmready === true){
-  var sendconfirm = confirm("you have selected a password of "+passlength+ " and containing"+ usedarr);
-  if(sendconfirm === true){
-    this.makepasword();
-  }
-  else{
-    document.getElementById("notifications").innerHTML = "you have cancelled making the password";
-  }
-}
-else{
+   }
+ 
+ if(document.getElementById("chklow").checked){
+  arrayofchar.push("lowercase");
   
+ }
+ if(document.getElementById("chknum").checked){
+  arrayofchar.push("numbers");
+  
+ }
+ if(document.getElementById("chkspec").checked){
+  arrayofchar.push("special");
+  
+ }
+ let aok = confirm("you have chosen a password containing "+arrayofchar+" and a length of "+passlength+ " press ok to confirm");
+  if(aok === true){
+    makepasword();
+  }
 }
+function makepasword (){
 
-
-}
-  function makepasword (){
-   let lowercase = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"];
-   let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-   let numbers = ["0","1","2","3","4","5","6","7","8","9"];
-   let special = ["!","@","#","$","%","^","&","*"," ",];//need more!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-   let usedarr = [];
-
-  for( let i=0; i < passlength; i++){
+   for( let i=0; i < passlength; i++){
     let order = Math.random();
-
+    let usedarr = [];
       if (order > 0.4999999){
         lowercase.reverse();
         uppercase.reverse();
@@ -70,169 +66,41 @@ else{
         special.reverse();
         console.log ("arrays were reversed");
       }
-      else {
-        console.log("arrays are normal");
+      if(document.getElementById("chkupp").checked){
+        usedarr.push(uppercase);
+      }
+      if(document.getElementById("chklow").checked){
+          usedarr.push(lowercase);  
+      }  
+      if(document.getElementById("chknum").checked){
+          usedarr.push(numbers);  
+      }
+      if(document.getElementById("chkspec").checked){
+          usedarr.push(special);
+      }
+      let order2 = Math.random;
+      if (order2 < 0.4999999){
+          usedarr.reverse();
+          console.log("usedarr was reversed");
       }
       console.log(usedarr);
-       passwordrtn += usedarr[Math.ceil(Math.random())][Math.ceil(Math.random())];
-       console.log(passwordrtn);
-  }
-
-}
-
-
-    
-     /* switch(true){
-        case document.getElementById("chkupp").checked && document.getElementById("chklow").checked && document.getElementById("chkspec").checked && document.getElementById("chknum").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(lowercase);
-          console.log("added lower");
-          usedarr.push(special);
-          console.log("added special");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked && document.getElementById("chklow").checked && document.getElementById("chkspec").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(lowercase);
-          console.log("added lower");
-          usedarr.push(special);
-          console.log("added special");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked && document.getElementById("chklow").checked && document.getElementById("chknum").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(lowercase);
-          console.log("added lower");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked && document.getElementById("chkspec").checked && document.getElementById("chknum").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(special);
-          console.log("added special");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chklow").checked && document.getElementById("chkspec").checked && document.getElementById("chknum").checked:
-          usedarr.push(lowercase);
-          console.log("added lower");
-          usedarr.push(special);
-          console.log("added special");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked && document.getElementById("chklow").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(lowercase);
-          console.log("added lower");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked && document.getElementById("chkspec").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(special);
-          console.log("added special");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked && document.getElementById("chknum").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chklow").checked && document.getElementById("chkspec").checked:
-          usedarr.push(lowercase);
-          console.log("added lower");
-          usedarr.push(special);
-          console.log("added special");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chklow").checked && document.getElementById("chknum").checked:
-          usedarr.push(lowercase);
-          console.log("added lower");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkspec").checked && document.getElementById("chknum").checked:
-          userarr.push(special);
-          console.log("added special");
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkupp").checked:
-          usedarr.push(uppercase);
-          console.log("added upper");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chklow").checked:
-          usedarr.push(lowercase);
-          console.log("added lower");
-          console.log(usedarr);
-          confirmready = true;
-          break;
-        case document.getElementById("chkspec").checked:
-          usedarr.push(special);
-          console.log("added special");
-          console.log(usedarr);
-          confirmready = true;
-
-          break;
-        case document.getElementById("chknum").checked:
-          usedarr.push(numbers);
-          console.log("added numbers");
-          console.log(usedarr);
-          confirmready = true;
-          this.areweconfirmready();
-          break;
-
-        default:
-        document.getElementById("notifications").innerHTML = document.getElementById("notifications").innerHTML +" please check the characters you wish to use.";
-        console.log("sent message to notification feild need characters input.");
+      charcheck = usedarr[Math.floor(Math.random()*usedarr.length)];
+      console.log(charcheck);
+      usedkey = charcheck[Math.floor(Math.random()*charcheck.length)];
+      console.log(usedkey);
+      if (typeof passwordrtn === "undefined"){
+          passwordrtn = usedkey;
+          console.log(passwordrtn);
+      } 
+      else{
+        passwordrtn += usedkey;
+        console.log(passwordrtn);
+      }  
     }
-  }*/
-
- 
-
-
-
-
+  writePassword();
+}
 // Write password to the #password input
-// original content
-
-    /*var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;*/
-
-
-
-
-
-
-
-// Add event listener to generate button
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  passwordText.value = passwordrtn;
+}
